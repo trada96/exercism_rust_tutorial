6c6f0c7d0f384ac123d030eba1849e0334d0d2dc
Luong Van Tuan Anh <luongvantuananh@gapo.com.vn>
2022-01-05 11:58:17 +0700

    add: reverse string

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -4,6 +4,5 @@ use chrono::{DateTime, Duration, Utc};
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
     const GIGASECOND: i64 = 1_000_000_000;
-    start + Duration::seconds(GIGASECOND)
-    
+    start + Duration::seconds(GIGASECOND)   
 }
