405fd83e3e3aca100ccfb32f4431293badd595e7
Luong Van Tuan Anh <luongvantuananh@gapo.com.vn>
2022-01-07 10:36:03 +0700

    add: grains

---
+++ rust/grains/src/lib.rs
@@ -0,0 +1,9 @@
+pub fn square(s: u32) -> u64 {
+    2_u64.pow(s-1)
+}
+
+pub fn total() -> u64 {
+    
+    (1..64+1).map(square).sum()
+
+}
---
+++ rust/grains/src/main.rs
@@ -0,0 +1,6 @@
+pub mod lib;
+
+fn main(){
+    lib::square(3);
+    lib::total();
+}
